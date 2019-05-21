9f4b3bd040b5b47ef73f43eaa86da02afa2b4bd1
Nytra <ch.nytra.13@gmail.com>
2019-05-21 20:48:45 +0200

    TestLogic INC

--- ClassLib/LogicFromVersion_1/WrongAnswer.cs
+++ ClassLib/LogicFromVersion_1/WrongAnswer.cs
@@ -1,24 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace IrregularVocabularyTest
+﻿namespace IrregularVocabularyTest
 {
     public class WrongAnswer
     {
-        public string Infinitiv { get; set; }
-        public string EnteredSimplePast { get; set; }
-        public string EnteredPastParticiple { get; set; }
-        public string EnteredTranslation { get; set; }
-        public int Key { get; set; }
-
         public WrongAnswer()
         {
             EnteredSimplePast     = string.Empty;
             EnteredPastParticiple = string.Empty;
             EnteredTranslation    = string.Empty;
         }
+
+        public string Infinitiv             { get; set; }
+        public string EnteredSimplePast     { get; set; }
+        public string EnteredPastParticiple { get; set; }
+        public string EnteredTranslation    { get; set; }
+        public int    Key                   { get; set; }
     }
 }
